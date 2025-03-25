a8f640c0e63a0696276b15331577821b8b768f2a
unknown <a.rochagabri@gmail.com>
2025-03-25 16:55:39 +0000

    SQL update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import mysql.connector
 import bcrypt
+import datetime
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="jasolvex25",
         database="solvex"
     )
 
