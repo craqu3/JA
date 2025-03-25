284c74d670e9832b232c60440a5b1d41c4f7e07e
unknown <a.rochagabri@gmail.com>
2025-03-25 23:12:10 +0000

    add js

---
+++ site/script.js
@@ -0,0 +1,7 @@
+var password = window.document.getElementById('password')
+var verPassword = window.document.getElementById('verpassword')
+
+if (verPassword != password){
+    window.alert('As passwords não coincidem')
+}
+
